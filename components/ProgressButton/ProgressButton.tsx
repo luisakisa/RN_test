2c11b800f80c5e605c98fb18608566facdb2d4ad
luisakisa <72152407+luisakisa@users.noreply.github.com>
2023-04-28 14:52:49 +0700

    #6_added_new_feature_for_progressbutton

--- components/ProgressButton/ProgressButton.tsx
+++ components/ProgressButton/ProgressButton.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {Text, View, Image, TouchableOpacity, TextInput} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Animated, {
+  useAnimatedProps,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
 import {styles} from './styles';
+const AnimatedText = Animated.createAnimatedComponent(TextInput)
 
 export default function ProgressButton() {
   const progress = useSharedValue(0);
+  let clicked: boolean = false;
 
-  function animateProgress(): void {
+  function animateProgressGrow(): void {
     progress.value = withTiming(100, {duration: 4000});
+    clicked = true;
+  }
+  function animateProgressLow(): void {
+    progress.value = withTiming(0, {duration: 4000});
+    clicked = false;
   }
 
   const gradientAnimatedStyleWidth = useAnimatedStyle(() => {
@@ -21,24 +29,33 @@ export default function ProgressButton() {
     };
   });
 
+
+  function buttonClick() {
+    clicked ? animateProgressLow() : animateProgressGrow();
+  }
+  const valueAnimatedProps = useAnimatedProps(() => {
+    return {text: `Unlocked only ${Math.round(progress.value)}% of content`} as any
+},[progress.value])
+
   return (
-      <TouchableOpacity style={styles.container} onPress={animateProgress}>
-        <Animated.View
-          style={[styles.gradientAnimatedStyle, gradientAnimatedStyleWidth]}>
-          <LinearGradient
-            colors={['#DD4E58', '#FF8158']}
-            style={styles.gradient}
-            start={{x: 0, y: 0.5}}
-            end={{x: 1, y: 0.5}}
-          />
-        </Animated.View>
-        <View style={styles.content}>
-          <View style={styles.textContainer}>
-            <Text style={styles.textUp}>Free User</Text>
-            <Text style={styles.textLow}>Unlocked only 32% of content</Text>
-          </View>
-          <Image source={require('./Vector.png')} style={styles.imgStyle} />
+    <TouchableOpacity style={styles.container} onPress={buttonClick}>
+      <Animated.View
+        style={[styles.gradientAnimatedStyle, gradientAnimatedStyleWidth]}>
+        <LinearGradient
+          colors={['#DD4E58', '#FF8158']}
+          style={styles.gradient}
+          start={{x: 0, y: 0.5}}
+          end={{x: 1, y: 0.5}}
+        />
+        
+      </Animated.View>
+      <View style={styles.content}>
+        <View style={styles.textContainer}>
+          <Text style={styles.textUp}>Free User</Text>
+          <AnimatedText value={'Unlocked only 0% of content'} editable={false} animatedProps={valueAnimatedProps} style={styles.textLow}/>
         </View>
-      </TouchableOpacity>
+        <Image source={require('./Vector.png')} style={styles.imgStyle} />
+      </View>
+    </TouchableOpacity>
   );
 }
