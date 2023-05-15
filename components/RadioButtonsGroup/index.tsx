80d5c0b72ca4fb4f63c2aea82f0a54669e69a24d
luisakisa <72152407+luisakisa@users.noreply.github.com>
2023-05-15 17:24:28 +0700

    #14 moved data

--- components/RadioButtonsGroup/index.tsx
+++ components/RadioButtonsGroup/index.tsx
@@ -2,29 +2,29 @@ import React, {useState} from 'react';
 import {View} from 'react-native';
 import RadioButton from '../RadioButton';
 import i18n from '../../i18n';
-('./../../i18n');
+
+const data = [
+  {
+    id: 1,
+    time: i18n.t('first.monthlyf'),
+    price: `$99 / ${i18n.t('first.month')}`,
+    days: 3,
+  },
+  {
+    id: 2,
+    time: `3 ${i18n.t('first.of-mounth')}`,
+    price: '$199',
+    days: 7,
+  },
+  {
+    id: 3,
+    time: `1 ${i18n.t('first.year')}`,
+    price: '$399',
+    days: 7,
+  },
+];
 
 export default function RadioButtonsGroup() {
-  const data = [
-    {
-      id: 1,
-      time: i18n.t('first.monthly'),
-      price: `$99 / ${i18n.t('first.month')}`,
-      days: 3,
-    },
-    {
-      id: 2,
-      time: `3 ${i18n.t('first.of-mounth')}`,
-      price: '$199',
-      days: 7,
-    },
-    {
-      id: 3,
-      time: `1 ${i18n.t('first.year')}`,
-      price: '$399',
-      days: 7,
-    },
-  ];
   const [selected, setSelected] = useState(1);
 
   const handleSelect = (id: number) => {
